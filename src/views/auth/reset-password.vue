940ab343476e902edcaad2fb141c4b49f76ccb90
Dave Njoroge <davenjorge10@gmail.com>
2025-02-14 01:34:16 +0300

    update password-reset page(wip)

--- src/views/auth/reset-password.vue
+++ src/views/auth/reset-password.vue
@@ -7,7 +7,7 @@
         and memorable!
       </auth-subtitle>
     </div>
-    <template v-if="params?.error">
+    <!-- <template v-if="params?.error">
       <base-alert variant="error" title="Invalid link">
         {{ params.error_description }}
       </base-alert>
@@ -21,27 +21,27 @@
       </div>
     </template>
 
-    <template v-else-if="loadingSession">
+<template v-else-if="loadingSession">
       <div class="py-10 text-center">
         <base-loader></base-loader>
       </div>
     </template>
 
-    <template v-else-if="validSession">
-      <Form @submit="updatePassword()">
-        <div class="space-y-4">
-          <form-input label="New password" v-model="password" type="password" :error="errors.password"
-            allow-password-toggle required></form-input>
-          <form-input label="Confirm password" v-model="confirmPassword" type="password"
-            :error="errors.password_confirmation" required></form-input>
-          <base-button type="submit" class="w-full" :loading="auth.loading">
-            <span>Update password</span>
-          </base-button>
-        </div>
-      </Form>
-    </template>
+<template v-else-if="validSession"> -->
+    <Form @submit="updatePassword()">
+      <div class="space-y-4">
+        <form-input label="New password" v-model="password" type="password" :error="errors.password"
+          allow-password-toggle required></form-input>
+        <form-input label="Confirm password" v-model="confirmPassword" type="password"
+          :error="errors.password_confirmation" required></form-input>
+        <base-button type="submit" class="w-full" :loading="auth.loading">
+          <span>Update password</span>
+        </base-button>
+      </div>
+    </Form>
+    <!-- </template> -->
 
-    <template v-else>
+    <!-- <template v-else>
       <div class="space-y-8">
         <div class="flex items-start space-x-3">
           <AlertCircle :size="32" />
@@ -56,7 +56,7 @@
           </base-button>
         </router-link>
       </div>
-    </template>
+    </template> -->
   </div>
 </template>
 <script lang="ts" setup>
@@ -64,29 +64,21 @@ import AuthTitle from '@/components/auth/title.vue'
 import AuthSubtitle from '@/components/auth/subtitle.vue'
 import FormInput from "@/components/form/input.vue"
 import BaseButton from "@/components/base/button.vue"
-import BaseAlert from "@/components/base/alert.vue"
-import BaseLoader from "@/components/base/loader.vue"
+// import BaseAlert from "@/components/base/alert.vue"
+// import BaseLoader from "@/components/base/loader.vue"
 import { useAuthStore } from "@/stores/auth"
 import { Form, useForm } from "vee-validate"
 import * as yup from "yup"
-import { computed, onMounted, ref } from "vue"
+import { computed, ref } from "vue"
 import { useRoute, useRouter } from "vue-router"
-import { AlertCircle, MoveRight } from "lucide-vue-next"
+// import { AlertCircle, MoveRight } from "lucide-vue-next"
 
 const auth = useAuthStore()
 const router = useRouter()
+const route = useRoute()
 
-const params = computed(() => {
-  const params = new URLSearchParams(useRoute().hash.substring(1)) // Use `hash` for Supabase URLs
-  const obj: Record<string, string> = {}
-  if (!params.entries()) {
-    return null
-  }
-  for (const [key, value] of params.entries()) {
-    obj[key] = value
-  }
-  return obj
-})
+const userId = computed(() => route.params.userId as string)
+const resetToken = computed(() => route.params.secret as string)
 
 const schema = yup.object({
   password: yup.string().min(6).required("Password is required"),
@@ -104,24 +96,16 @@ const [password] = defineField("password")
 const [confirmPassword] = defineField("password_confirmation")
 
 const validSession = ref<boolean>(false)
-const loadingSession = ref(false)
+// const loadingSession = ref(false)
 
 const updatePassword = handleSubmit(async (values) => {
   if (!validSession.value) {
     return
   }
 
-  const success = await auth.updatePassword(values.password)
+  const success = await auth.updatePassword(userId.value, resetToken.value, values.password)
   if (success) {
     router.push("/login")
   }
-})
-
-onMounted(async () => {
-  loadingSession.value = true
-
-  validSession.value = await auth.getSession()
-
-  loadingSession.value = false
 })
 </script>
