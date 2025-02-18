529a9b049eb9ee9c7d80220e53df0b860baedc6a
Dave Njoroge <davenjoroge10@gmail.com>
2025-02-18 03:39:24 +0300

    updated project views structure

--- src/router/routes/projects.ts
+++ src/router/routes/projects.ts
@@ -5,37 +5,47 @@ export default [
     // default projects page is tasks
     component: () => import('@/views/projects/index.vue'),
   },
-
   {
-    path: '/projects/:id',
+    path: '/projects/view/:id',
     // default projects page is tasks
-    component: () => import('@/views/projects/[id].vue'),
+    component: () => import('@/views/projects/view/[id].vue'),
     children: [
       {
         path: '', // overview -> default
         name: 'project',
-        component: () => import('@/views/projects/[id]/index.vue'),
+        component: () => import('@/views/projects/view/[id]/index.vue'),
       },
       {
         path: 'tasks',
         name: 'project-tasks',
-        component: () => import('@/views/projects/[id]/tasks.vue'),
+        component: () => import('@/views/projects/view/[id]/tasks.vue'),
       },
       {
         path: 'members',
         name: 'project-members',
-        component: () => import('@/views/projects/[id]/members.vue'),
+        component: () => import('@/views/projects/view/[id]/members.vue'),
       },
       {
         path: 'events',
         name: 'project-events',
-        component: () => import('@/views/projects/[id]/events.vue'),
+        component: () => import('@/views/projects/view/[id]/events.vue'),
       },
       {
         path: 'about',
         name: 'project-about',
-        component: () => import('@/views/projects/[id]/about.vue'),
+        component: () => import('@/views/projects/view/[id]/about.vue'),
       },
     ],
+  },
+
+  {
+    path: '/projects/new',
+    name: 'create-project',
+    component: () => import('@/views/projects/create.vue'),
+  },
+  {
+    path: '/projects/update/:id',
+    name: 'update-project',
+    component: () => import('@/views/projects/update.vue'),
   },
 ]
