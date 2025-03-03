c8255b9a26b7fa6ab67b1df96b7cf98e5ab8680a
Dave Njoroge <davenjoroge10@gmail.com>
2025-03-03 18:16:36 +0300

    added event store/service logic

---
+++ src/services/events.ts
@@ -0,0 +1,68 @@
+import { supabase } from '@/database/supabase'
+import type { ProjectEventForm } from '@/stores/event'
+
+export const useEventService = () => {
+  const list = async (project: string) => {
+    // event
+    return await supabase.from('events').select().eq('project_id', project)
+  }
+
+  const getEvent = async (id: string) => {
+    return await supabase.from('events').select().eq('id', id)
+  }
+
+  const create = async (form: ProjectEventForm) => {
+    return await supabase.from('events').insert(form).select()
+  }
+
+  const update = async (id: string, form: ProjectEventForm) => {
+    return await supabase.from('events').insert(form).eq('id', id)
+  }
+
+  const destroy = async (id: string) => {
+    return await supabase.from('events').delete().eq('id', id)
+  }
+
+  /**
+   * USER
+   */
+
+  const getUserEvents = async (user_id: string) => {
+    return await supabase.from('event_invitees').select().eq('user_id', user_id)
+  }
+
+  /**
+   * INVITEES
+   */
+
+  const addInvitees = async (payload: { user_id: string; event_id: string }[]) => {
+    return await supabase.from('event_invitees').insert(payload).select()
+  }
+
+  const getInvitees = async (event_id: string) => {
+    return await supabase.from('event_invitees').select().eq('event_id', event_id)
+  }
+
+  const deleteInvitee = async (event_id: string, user_id: string) => {
+    return await supabase
+      .from('event_invitees')
+      .delete()
+      .eq('event_id', event_id)
+      .eq('user_id', user_id)
+  }
+
+
+  return {
+    list,
+    getEvent,
+    create,
+    update,
+    destroy,
+
+    getUserEvents,
+
+    addInvitees,
+    getInvitees,
+    deleteInvitee,
+  }
+}
--- src/stores/event.ts
+++ src/stores/event.ts
@@ -1,9 +1,253 @@
 import { acceptHMRUpdate, defineStore } from 'pinia'
+import { useAuthStore } from './auth'
+import { useErrorStore } from './errors'
+import { useEventService } from '@/services/events'
+import { useLoadingState } from '@/composables/useLoading'
 
-export const useEventStore = defineStore('project_events', () => {}, {
-  persist: false,
-})
+export enum EventLoading {
+  GETTING = 'getting-events',
+  GETTING_ONE = 'getting-event',
+  CREATING = 'creating-event',
+  UPDATING = 'updating-event',
+  DELETING = 'deleting-event',
+  ADDING_INVITEES ='adding-invitees',
+  DELETING_INVITEE = 'deleting-invitee',
+  GETTING_USER = 'getting-user-events',
+}
+
+export const useEventStore = defineStore(
+  'project_events',
+  () => {
+    const service = useEventService()
+    const auth = useAuthStore()
+    const { handleError } = useErrorStore()
+    const { begin, finish } = useLoadingState()
+
+    const ensureAuth = () => {
+      if (!auth.isAuthenticated) {
+        throw new Error('User is not authenticated')
+      }
+
+      return true
+    }
+
+    const getEvents = async (project: string) => {
+      ensureAuth()
+      try {
+        begin(EventLoading.GETTING)
+        const { error, data } = await service.list(project)
+        if (error) throw new Error(error.message)
+
+        if (data) {
+          return data[0]
+        }
+
+        return null
+      } catch (error) {
+        handleError('Fetching events', error)
+      } finally {
+        finish(EventLoading.GETTING)
+      }
+    }
+
+    const getEvent = async (id: string) => {
+      ensureAuth()
+      try {
+        begin(EventLoading.GETTING_ONE)
+        const { data, error } = await service.getEvent(id)
+        if (error) throw new Error(error.message)
+
+        if (data) {
+          return data[0]
+        }
+
+        return null
+      } catch (error) {
+        handleError('Fetching event', error)
+      } finally {
+        finish(EventLoading.GETTING_ONE)
+      }
+    }
+
+    const create = async (form: ProjectEventForm, invitees: string[]) => {
+      ensureAuth()
+      try {
+        begin(EventLoading.CREATING)
+        const { data, error } = await service.create(form)
+        if (error) throw new Error(error.message)
+
+        if (data) {
+          // add creator to invitees
+          invitees.push(form.created_by)
+
+          await addInvitees(data[0].id, invitees)
+          return data[0]
+        }
+
+        return null
+      } catch (error) {
+        handleError('Creating event', error)
+      } finally {
+        finish(EventLoading.CREATING)
+      }
+    }
+
+    const update = async (id: string, form: ProjectEventForm) => {
+      ensureAuth()
+      try {
+        begin(EventLoading.UPDATING)
+        const { status, error } = await service.update(id, form)
+        if (error) throw new Error(error.message)
+
+        if (status === 204) {
+          return true
+        }
+
+        return false
+      } catch (error) {
+        handleError('Updating event', error)
+      } finally {
+        finish(EventLoading.UPDATING)
+      }
+    }
+
+    const destroy = async (id: string) => {
+      ensureAuth()
+      try {
+        begin(EventLoading.DELETING)
+        const { status, error } = await service.destroy(id)
+        if (error) throw new Error(error.message)
+
+        if (status === 204) {
+          return true
+        }
+
+        return false
+      } catch (error) {
+        handleError('Deleting event', error)
+      } finally {
+        finish(EventLoading.DELETING)
+      }
+    }
+
+    /**
+     * USER
+     */
+
+    const getUserEvents = async (user_id: string) => {
+      try {
+        begin(EventLoading.GETTING_USER)
+        const { error, data } = await service.getUserEvents(user_id)
+        if (error) throw new Error(error.message)
+        if (data) {
+          return data
+        }
+
+        return null
+      } catch (error) {
+        handleError('Fetching user events', error)
+      } finally {
+        finish(EventLoading.GETTING_USER)
+      }
+    }
+
+    /**
+     * INVITEES
+     */
+
+    const addInvitees = async (event_id: string, invitees: string[]) => {
+      ensureAuth()
+      try {
+        begin(EventLoading.ADDING_INVITEES)
+        const payload = invitees.map((user_id) => ({
+          event_id,
+          user_id,
+        }))
+
+        const { error, status } = await service.addInvitees(payload)
+
+        if (error) throw new Error(error.message)
+        if (status === 204) return true
+
+        return false
+      } catch (error) {
+        handleError('Adding invitees', error)
+      }finally {
+        finish(EventLoading.ADDING_INVITEES)
+      }
+    }
+
+    const deleteInvitee = async (event_id: string, user_id: string) => {
+      ensureAuth()
+      try {
+        begin(EventLoading.DELETING_INVITEE)
+        const { error, status } = await service.deleteInvitee(event_id, user_id)
+        if (error) throw new Error(error.message)
+        if (status === 204) return true
+        return false
+      } catch (error) {
+        handleError('Deleting invitee', error)
+      }
+      finally {
+        finish(EventLoading.DELETING_INVITEE)
+      }
+    }
+
+    return {
+      create,
+      getEvent,
+      getEvents,
+      update,
+      destroy,
+
+      getUserEvents,
+
+      // invitees
+      addInvitees,
+      deleteInvitee,
+    }
+  },
+  {
+    persist: false,
+  },
+)
 
 if (import.meta.hot) {
   import.meta.hot.accept(acceptHMRUpdate(useEventStore, import.meta.hot))
+}
+
+export interface ProjectEvent {
+  id: string
+  project_id: string
+  title: string
+  description: string | null
+  created_at: string
+  created_by: string
+  url: string | null
+  venue: string | null
+  event_type: string
+  status: string
+  date: string
+  time: string | null
+  duration_hours: number | null
+  updated_at: string | null
+  closed_at: string | null
+}
+
+export interface ProjectEventForm {
+  id?: string
+  project_id: string
+  title: string
+  description?: string | null
+  created_at: string
+  created_by: string
+  url: string | null
+  venue?: string | null
+  event_type: string
+  status: string
+  date: string
+  time?: string | null
+  duration_hours?: number | null
+  updated_at?: string | null
+  closed_at?: string | null
 }
