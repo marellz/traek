7bc2a963d3e31b93a421e0c98b4b5db261f21b98
Dave Njoroge <davenjoroge10@gmail.com>
2025-03-10 14:29:33 +0300

    design fixes

--- components/layout/card.vue
+++ components/layout/card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-sidebar-bg rounded-lg border border-slate-200 p-3 sm:p-4 md:p-8">
+  <div class="bg-white rounded-lg border border-slate-200 p-3 sm:p-4 md:p-8">
     <slot />
   </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- dashboard -->
   <div class="flex h-full min-h-screen flex-col">
-    <div class="bg-sidebar-bg fixed left-0 h-full w-1/5" />
+    <div class="bg-sidebar-bg fixed left-0 h-full w-1/5 max-w-64 z-1" />
     <div class="container mx-auto flex flex-auto space-x-2">
       <partials-nav />
       <div class="flex flex-auto flex-col">
