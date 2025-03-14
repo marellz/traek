c2d3fb469f6863390f55d3dcf788834663b18dba
Dave Njoroge <davenjoroge10@gmail.com>
2025-03-14 19:28:50 +0300

    update btn-action for darkmode

--- src/components/base/action.vue
+++ src/components/base/action.vue
@@ -1,7 +1,7 @@
 <template>
   <button :type @click="onClick" ref="btn"
     :disabled="confirmed"
-    class="relative rounded-lg border overflow-hidden border-slate-400 dark:border-slate-400 px-2 py-2 text-sm leading-none font-medium text-slate-800 dark:text-slate-400 hover:border-slate-800 dark:hover:bg-slate-800 dark:hover:border-slate-500 disabled:text-slate-400 disabled:bg-slate-100 disabled:border-slate-400">
+    class="relative rounded-lg border overflow-hidden border-slate-400 dark:border-slate-400 px-2 py-2 text-sm leading-none font-medium text-slate-800 dark:text-slate-400 hover:border-slate-800 dark:hover:bg-slate-800 dark:hover:border-slate-500 disabled:text-slate-400 disabled:bg-slate-100 disabled:border-slate-400 dark:disabled:bg-slate-700">
 
     <div v-show="confirmMode" class="absolute inset-0 flex items-center justify-center" :class="[confirmColor]">
       <p>Confirm?</p>
