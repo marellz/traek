5d36b04e756931ef0b666f3202873795002c5dc9
Dave Njoroge <davenjoroge10@gmail.com>
2025-03-17 21:59:20 +0300

    update loader, structure simplified

--- src/components/base/loader.vue
+++ src/components/base/loader.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="py-4 text-center">
-    <div class="loader inline-block dark:text-white">
-      <svg :class="[loaderClass]" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <path d="M21 12a9 9 0 1 1-6.219-8.56"></path>
-      </svg>
-    </div>
+  <div class="text-center dark:text-white">
+    <svg :class="[loaderClass]" class="loader inline-block h-6 w-6" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+      viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+      stroke-linejoin="round">
+      <path d="M21 12a9 9 0 1 1-6.219-8.56"></path>
+    </svg>
   </div>
 </template>
 <script lang="ts" setup>
