ef121b2d10f473ccbc982f9ff890a7881a21aaad
Dave Njoroge <davenjoroge10@gmail.com>
2025-03-18 23:58:22 +0300

    update user avatar and menu components

--- src/components/user/avatar.vue
+++ src/components/user/avatar.vue
@@ -1,28 +1,21 @@
 <template>
-  <img v-if="avatar" class="object-center object-cover rounded-full border-2" :class="sizes[size]" :src="avatar"
-    alt="" />
+  <img v-if="avatar" class="object-center object-cover rounded-full border-2" :class="size" :src="avatar" alt="" />
   <span v-else class="rounded-full border-2 border-white/10 bg-white/20 inline-flex items-center justify-center"
-    :class="sizes[size]">
-    <User2 :size="30" :stroke-width="1" />
+    :class="size">
+    <User :size="iconSize" :stroke-width="1" />
   </span>
 </template>
 <script lang="ts" setup>
-import { User2 } from "lucide-vue-next"
-type sizes = "lg" | "md" | "sm"
-type sizeClasses = Record<sizes, string>
+import { User } from "lucide-vue-next"
 withDefaults(
   defineProps<{
     avatar: string | null | undefined
-    size?: sizes
+    size?: string
+    iconSize?: number
   }>(),
   {
-    size: "md",
+    iconSize: 24,
+    size: "h-6 w-6",
   },
 )
-
-const sizes: sizeClasses = {
-  sm: "h-9 w-9",
-  md: "h-12 w-12",
-  lg: "h-16 w-16",
-}
 </script>
--- src/components/user/menu.vue
+++ src/components/user/menu.vue
@@ -6,8 +6,8 @@
     <div v-show="active"
       class="absolute top-full right-0 w-auto min-w-40 overflow-hidden rounded-md border border-slate-300 bg-white dark:bg-slate-700 dark:border-slate-600 z-10">
       <div class="flex flex-col">
-        <div class="flex items-center space-x-2 bg-slate-100 dark:bg-slate-700 p-2">
-          <user-avatar size="sm" :avatar="auth.profile?.avatar"></user-avatar>
+        <div class="flex items-center space-x-2 bg-slate-100 dark:bg-slate-700 p-2 border-b border-b-slate-200 dark:border-b-slate-600">
+          <user-avatar size="h-10 w-10" :avatar="auth.profile?.avatar"></user-avatar>
           <div>
             <p class="font-medium text-sm">
               {{ auth.profile?.name || 'User name' }}
@@ -15,11 +15,15 @@
           </div>
         </div>
 
-        <router-link to="/profile/user-id" class="block p-2 hover:bg-slate-100 dark:hover:bg-slate-600">
-          Profile
-        </router-link>
-        <a href="#logout" class="block p-2 hover:bg-slate-100 dark:hover:bg-slate-600" @click.prevent="logout"> Logout
-        </a>
+        <div class="text-sm">
+          <router-link :to="{ name: 'view-profile', params: { id: auth.userId } }"
+            class="block p-2 hover:bg-slate-100 dark:hover:bg-slate-600">
+            Profile
+          </router-link>
+          <a href="#logout" class="block p-2 hover:bg-slate-100 dark:hover:bg-slate-600" @click.prevent="logout"> Logout
+          </a>
+        </div>
+
       </div>
     </div>
   </div>
@@ -29,11 +33,12 @@ import UserAvatar from '@/components/user/avatar.vue'
 import { useAuthStore } from '@/stores/auth'
 import { onClickOutside } from '@vueuse/core'
 import { User } from 'lucide-vue-next'
-import { ref, useTemplateRef } from 'vue'
-import { useRouter } from 'vue-router'
+import { ref, useTemplateRef, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 
 const auth = useAuthStore()
 const router = useRouter()
+const route = useRoute()
 
 const logout = async () => {
   if (confirm('Are you sure you want to log out?')) {
@@ -45,6 +50,10 @@ const logout = async () => {
 const active = ref(false)
 const wrapper = useTemplateRef('target')
 onClickOutside(wrapper, () => {
+  active.value = false
+})
+
+watch(() => route.path, () => {
   active.value = false
 })
 </script>
