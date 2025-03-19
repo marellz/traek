40e49ad9a7c4272ec513514d4a20b32e06cb5b8f
Dave Njoroge <davenjoroge10@gmail.com>
2025-03-19 22:56:14 +0300

    update base-action

--- src/components/base/action.vue
+++ src/components/base/action.vue
@@ -1,9 +1,10 @@
 <template>
   <button :type @click="onClick" ref="btn"
     :disabled="confirmed"
-    class="relative rounded-lg border overflow-hidden border-slate-400 dark:border-slate-400 px-2 py-2 text-sm leading-none font-medium text-slate-800 dark:text-slate-400 hover:border-slate-800 dark:hover:bg-slate-800 dark:hover:border-slate-500 disabled:text-slate-400 disabled:bg-slate-100 disabled:border-slate-400 dark:disabled:bg-slate-700">
+    class="relative inline-flex items-center space-x-2 rounded-lg border overflow-hidden border-slate-400 dark:border-slate-400 px-2 py-2 text-sm leading-none font-medium text-slate-800 dark:text-slate-400 hover:border-slate-800 dark:hover:bg-slate-800 dark:hover:border-slate-500 disabled:text-slate-400 disabled:bg-slate-100 disabled:border-slate-400 dark:disabled:bg-slate-700"
+    :class="{'!border-red-500': confirmMode}">
 
-    <div v-show="confirmMode" class="absolute inset-0 flex items-center justify-center" :class="[confirmColor]">
+    <div v-show="confirmMode" class="absolute inset-0 flex items-center justify-center mr-0" :class="[confirmColor]">
       <p>Confirm?</p>
     </div>
     <slot />
