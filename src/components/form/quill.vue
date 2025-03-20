bc7df3d697b6aca006592eebc2b5b776d81ff311
Dave Njoroge <davenjoroge10@gmail.com>
2025-03-20 14:41:50 +0300

    added vue-quill, created quill "wysiwyg" component

---
+++ src/components/form/quill.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <quill-editor theme="snow" content-type="html" v-model:content="model"></quill-editor>
+    <form-error v-if="error" class="mt-1">{{ error }}</form-error>
+  </div>
+</template>
+<script lang="ts" setup>
+import FormError from "@/components/form/error.vue"
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import '@/assets/style/components/quill.css'
+defineProps<{
+  error?: string
+}>()
+
+const model = defineModel<any>()
+
+</script>
