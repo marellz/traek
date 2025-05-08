ba39bb872217afef07394fb7e6256eead4c9e9a3
Dave Njoroge <davenjoroge10@gmail.com>
2025-05-08 21:50:41 +0300

    added dark mode component

---
+++ src/components/app/dark-mode.vue
@@ -0,0 +1,13 @@
+<template>
+  <button type="button" class="p-1" @click="toggleDarkMode()">
+    <Sun v-if="isDark" />
+    <Moon v-else />
+  </button>
+</template>
+<script lang="ts" setup>
+import { Sun, Moon } from 'lucide-vue-next'
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDarkMode = useToggle(isDark)
+</script>
