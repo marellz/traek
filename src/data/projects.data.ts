08163a6cbaccdd36cb27e82ea08f945e980e532c
Dave Njoroge <davenjoroge10@gmail.com>
2025-05-12 23:28:06 +0300

    new project data/variables

---
+++ src/data/projects.data.ts
@@ -0,0 +1,58 @@
+export enum ProjectCategories {
+  GENERAL = 'general',
+  PERSONAL = 'personal',
+  WORK = 'work',
+  SOFTWARE_DEVELOPMENT = 'software_development',
+  EVENT_PLANNING = 'event_planning',
+  RESEARCH = 'research',
+  MARKETING_CAMPAIGN = 'marketing_campaign',
+  CONSTRUCTION = 'construction',
+  EDUCATION = 'education',
+  CREATIVE = 'creative',
+  COMMUNITY = 'community',
+  FREElANCE = 'freelance',
+}
+
+export type ProjectCategory = `${ProjectCategories}`
+
+export const projectCategoryLabels = {
+  [ProjectCategories.GENERAL]: 'General',
+  [ProjectCategories.PERSONAL]: 'Personal',
+  [ProjectCategories.WORK]: 'Work',
+  [ProjectCategories.SOFTWARE_DEVELOPMENT]: 'Software development',
+  [ProjectCategories.EVENT_PLANNING]: 'Event planning',
+  [ProjectCategories.RESEARCH]: 'Research',
+  [ProjectCategories.MARKETING_CAMPAIGN]: 'Marketing ampaign',
+  [ProjectCategories.CONSTRUCTION]: 'Construction',
+  [ProjectCategories.EDUCATION]: 'Education',
+  [ProjectCategories.CREATIVE]: 'Creative',
+  [ProjectCategories.COMMUNITY]: 'Community',
+  [ProjectCategories.FREElANCE]: 'Freelance',
+}
+
+export const projectCategoryDescription = {
+  [ProjectCategories.GENERAL]: 'Default catch-all project',
+  [ProjectCategories.PERSONAL]: 'Personal goal tracking, habits, etc.',
+  [ProjectCategories.WORK]: 'Generic work projects',
+  [ProjectCategories.SOFTWARE_DEVELOPMENT]: 'Product builds, sprints, bug tracking',
+  [ProjectCategories.EVENT_PLANNING]: 'Organizing weddings, conferences, meetups',
+  [ProjectCategories.RESEARCH]: 'Academic, scientific, or field research projects',
+  [ProjectCategories.MARKETING_CAMPAIGN]: 'Campaigns, content calendars',
+  [ProjectCategories.CONSTRUCTION]: 'Building or renovation projects',
+  [ProjectCategories.EDUCATION]: 'Courses, tutoring, lesson planning',
+  [ProjectCategories.CREATIVE]: 'Design, writing, filmmaking',
+  [ProjectCategories.COMMUNITY]: 'SACCO, church, school, or NGO activities',
+  [ProjectCategories.FREElANCE]: 'Client work, gigs, contract jobs',
+}
+
+export enum ProjectPriorities {
+  LOW = 'low',
+  MEDIUM = 'medium',
+  HIGH = 'high',
+}
+
+export const projectPriorities = {
+  [ProjectPriorities.LOW]: 'Low',
+  [ProjectPriorities.MEDIUM]: 'Medium',
+  [ProjectPriorities.HIGH]: 'High'
+}
