0982c8da9f0a68e768f0dd8d302d6edfe3dda3f3
Dave Njoroge <davenjoroge10@gmail.com>
2025-05-13 18:12:23 +0300

    remove project update page

--- src/views/projects/update.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <project-form @submit="submit"></project-form>
-</template>
-<script lang="ts" setup>
-import ProjectForm from '@/components/object-forms/project.vue'
-const submit = () => {}
-</script>
