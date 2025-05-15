0fb763b9398ed8a431d38089cf0e323ab2661211
Dave Njoroge <davenjoroge10@gmail.com>
2025-05-15 12:13:27 +0300

    update project-form team page

--- src/views/projects/form/team.vue
+++ src/views/projects/form/team.vue
@@ -3,65 +3,86 @@
     <legend>Project users</legend>
     <div class="space-y-8">
       <div class="space-y-4">
-        <form-user-selector label="Members" v-model="members" :queried-users
-          @search-users="searchUsers"></form-user-selector>
+        <h2 class="font-medium">Current members</h2>
+        <!-- todo: creatively limit this list to 5, and a search for it only -->
+        <ul class="space-y-2">
+          <li v-for="(user, index) in currentMembers" :key="index">
+            <div class="flex space-x-2 items-center">
+              <Avatar :avatar="user.avatar" size="h-8 w-8"></Avatar>
+              <div class="flex-auto">
+                <div class="flex items-center space-x-2">
+                  <h1 class="font-medium">{{ user.name || 'No name' }}</h1>
+                  <base-tag class="text-xs">
+                    {{ user.role }}
+                  </base-tag>
+                </div>
+                <p class="text-sm text-slate-500">{{ user.email }}</p>
+              </div>
+              <button type="button" class="p-1 hover:text-red-500" @click="removeMember(user.user_id)">
+                <X />
+              </button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="space-y-4">
+        <form-user-selector :label="userSelectorLabel" v-model="members" :exclude></form-user-selector>
       </div>
       <div class="flex justify-end">
         <base-button @click="submit">
-          <span>Next</span>
-          <ArrowRight :size="20" />
+          <span>Add members</span>
+          <Plus />
         </base-button>
       </div>
     </div>
   </fieldset>
 </template>
 <script lang="ts" setup>
-import { useProjectStore, type ProjectUser } from '@/stores/project'
+import { useProjectStore, type ProjectMember } from '@/stores/project'
 import FormUserSelector from '@/components/form/user-selector.vue'
-import { useUserStore } from '@/stores/user'
-import { useDebounceFn } from '@vueuse/core'
 import { computed, onMounted, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
-import { ArrowRight } from 'lucide-vue-next'
+import { Plus, X } from 'lucide-vue-next'
 import { useProjectFormStore } from '@/stores/project.form'
-const userStore = useUserStore()
+import type { UserRole } from '@/data/users.data'
+import Avatar from '@/components/user/avatar.vue'
 const route = useRoute()
 const id = computed(() => route.params.id as string)
-const members = ref<string[]>([])
-const oldMembers = ref<string[]>([])
-const newMembers = computed(() => members.value.filter(m => !oldMembers.value.includes(m)))
-const queriedUsers = ref<ProjectUser[]>([])
-
-const searchUsers = useDebounceFn(async (q: string) => {
-  if (q == '') {
-    queriedUsers.value = []
-    return
-  }
-  const _users = await userStore.queryUsers(q)
-  if (_users && _users.length) {
-    queriedUsers.value = _users
-  }
-}, 1000)
+const members = ref<{ user_id: string, role: UserRole }[]>([])
+const currentMembers = ref<ProjectMember[]>([])
+const exclude = computed(() => currentMembers.value.map(m => m.id))
 
 const projectStore = useProjectStore()
 const projectFormStore = useProjectFormStore()
-onMounted(async () => {
+
+const userSelectorLabel = computed(() => currentMembers.value.length > 0 ? 'Add more team members' : 'Add your team members')
+
+const getCurrentMembers = async () => {
   const _members = await projectStore.getMembers(id.value)
 
   if (_members) {
-    members.value = _members.map((m) => m.id)
-    oldMembers.value = members.value
+    currentMembers.value = _members
+  }
+}
+
+const removeMember = async (user_id: string) => {
+  if(!confirm('Are you sure you want to remove this member?')) return
+  const _deleted = await projectStore.removeMember(user_id, id.value)
+  if (_deleted) {
+    const _index = currentMembers.value.findIndex(m => m.user_id === user_id)
+    if (_index !== -1) currentMembers.value.splice(_index, 1)
   }
 
-})
+}
+
+onMounted(getCurrentMembers)
 
 const router = useRouter()
 const submit = async () => {
-  const success = await projectStore.addMembers(id.value, newMembers.value)
+  const success = await projectStore.addMembers(id.value, members.value)
   if (success) {
-
-    // todo: add these members manually.
     await projectFormStore.getProject()
+    members.value = []
     router.push({ name: 'project-form-review', params: { id: id.value } })
   }
 }
